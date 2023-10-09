fe86a8146dc6ca54bf6feaff60b51edf1eb4fea8
Jhon Rey <jhonreymendiola@gmail.com>
2023-10-09 13:30:55 +0800

    bill api and pdf

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var cors = require('cors')
 const connection = require('./connection');
 const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
-const productRoute = require('./routes/product')
+const productRoute = require('./routes/product');
+const billRoute = require('./routes/bill');
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/category', categoryRoute);
-app.use('/product', productRoute)
+app.use('/product', productRoute);
+app.use('/bill', billRoute);
 
 module.exports = app;
